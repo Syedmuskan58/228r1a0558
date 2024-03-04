a4a9c874c026bc0cfc92aa6f90936ecf49baf1bd
Syedmuskan58 <161117084+Syedmuskan58@users.noreply.github.com>
2024-03-04 23:07:41 +0530

    ifdemo

---
+++ ifdemo.py
@@ -0,0 +1,13 @@
+#n=int(input("enter the n value"))
+#if n%2==0:
+ #    print(n,'is even')
+#else:
+    #print(n,'is odd')
+a=int(input("enter a value"))
+print(a**2)
+for i in range(1,11):
+    print(i)
+k=10
+while k<200 :
+     print(k)
+     k=k+1
