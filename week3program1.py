0c5997715b80a2097cad8ee9712a76be1716556b
Syedmuskan58 <161117084+Syedmuskan58@users.noreply.github.com>
2024-03-13 11:01:36 +0530

    convert list and tuple into arrays

---
+++ week3program1.py
@@ -0,0 +1,10 @@
+import numpy as np
+list_data=[1,2,3,4,5]
+array1=np.array(list_data)
+tupple_data=(6,7,8,9,10)
+array2=np.array(tupple_data)
+print(list_data)
+print("after converting list into array")
+print(array1)
+print("after connverting yuplr into array")
+print(array2)
