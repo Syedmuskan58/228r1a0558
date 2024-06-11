d273dd03a202994f7bd2bd573e0f0cb9d8c6fc97
Syedmuskan58 <161117084+Syedmuskan58@users.noreply.github.com>
2024-06-11 13:45:05 +0530

    week 4.3

---
+++ week4program3.py
@@ -0,0 +1,51 @@
+'''def commas(str):
+    x=str.replace('',',')[1:-1]
+    return x
+    #print(word[:-1])
+print(repr(commas("Apple")))'''
+
+'''def remove_word(str,word):
+    str1=str.replace(word,'')
+    return str1
+str2=("hello good morning hi hello good")
+print("before remove")
+print(str2)
+print("after remove")
+str3=remove_word(str2,"hello")
+print(str3)'''
+
+'''print(ord('&'))
+print(ord('*'))
+print(ord('+'))
+print(ord('!'))
+print(ord('~'))
+print(chr(0))
+print(chr(57))
+print(chr(1))
+print(chr(2))
+print(chr(72))
+print(chr(11))
+print(chr(33))
+print(chr(34))
+print(chr(35))
+print(chr(36))
+print(chr(37))
+print(ord('a'))'''
+
+'''str="hello good morning welcome to cmr engineering college"
+ch=list(str)
+print(ch)'''
+
+'''sentence="hello good morning welcome to cmr engineering college"'''
+'''def convert(str):
+    ch=list(str)
+    for i in range(len(str)):
+        if (i==0 and ch[i]!='  '):
+            if(ch[i]>='a' and ch[i]<='z'):
+                ch[i]=chr(ord(ch[i])-ord('a')+ord('A'))
+
+        str1="",ch[i]
+    return str1
+
+print(convert("hello good morning"))'''
+
