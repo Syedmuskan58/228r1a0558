52d0e775769f18261368bdbb6d16b577a8642298
Syedmuskan58 <161117084+Syedmuskan58@users.noreply.github.com>
2024-06-20 10:27:59 +0530

    test module

---
+++ testmodule.py
@@ -0,0 +1,18 @@
+'''import Arithmetic
+from Arithmetic import *'''
+from factorial import *
+'''print(Arithmetic.add(10,20))
+print(Arithmetic.sub(20,40))
+print(Arithmetic.mul(10,20))
+print(add(10,20))
+print(sub(15,40))
+print(mul(5,12))
+'''
+'''x=factorial(5)
+print("factorial of ",x)'''
+list_1=[]
+n=int(input("enter no of elements"))
+for i in range(0,n):
+    #x=int(input("enter the value"))
+    list_1.append(int(input("enter the value in the list")))
+print(list_1)
